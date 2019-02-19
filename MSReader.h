a77abfd20b52506d1416c1ccdb11f590be66a28b
Darren Enos <darren@primelabs.ms>
2019-02-19 11:54:14 -0700

    changing files to play nice

--- MSReader.h
+++ MSReader.h
@@ -30,20 +30,20 @@ limitations under the License.
 
 #ifdef _MSC_VER
 #ifndef _NO_THERMORAW
-#import "libid:F0C5F3E3-4F2A-443E-A74D-0AABE3237494" rename_namespace("XRawfile")
+//#import "libid:F0C5F3E3-4F2A-443E-A74D-0AABE3237494" rename_namespace("XRawfile")
 //#import "libid:5FE970A2-29C3-11D3-811D-00104B304896" rename_namespace("XRawfile")
-using namespace XRawfile;
+//using namespace XRawfile;
 #endif
 #endif
 
-#ifndef _NOSQLITE
-#include <sqlite3.h>
-#endif
+// #ifndef _NOSQLITE
+// #include <sqlite3.h>
+// #endif
 
 //Macros for 64-bit file support
 #ifdef _MSC_VER
 #ifndef _NO_THERMORAW
-#include "RAWReader.h"
+//#include "RAWReader.h"
 #endif
 typedef __int64 f_off;
 #define fseek(h,p,o) _fseeki64(h,p,o)
@@ -58,7 +58,7 @@ typedef off_t f_off;
 #define fseek(h,p,o) fseeko(h,p,o)
 #define ftell(h) ftello(h)
 
-#endif /* end _MSC_VER */ 
+#endif /* end _MSC_VER */
 
 
 using namespace std;
@@ -76,7 +76,7 @@ class MSReader {
   void appendFile(char* c, bool text, MSObject& m);
   void appendFile(char* c, Spectrum& s);
   void appendFile(char* c, MSObject& m);
-  
+
   MSFileFormat checkFileFormat(const char *fn);
 
   string          getCurrentFile();
@@ -86,7 +86,7 @@ class MSReader {
   int             getLastScan();
   void            getManufacturer(char* str);
   int             getPercent();
-  
+
 
   //get specific header informations
   void setPrecision(int i, int j);
@@ -99,16 +99,16 @@ class MSReader {
   bool readMSTFile(const char* c, bool text, Spectrum& s, int scNum=0);
   bool readMZPFile(const char* c, Spectrum& s, int scNum=0);
   bool readFile(const char* c, Spectrum& s, int scNum=0);
-  
+
   void setFilter(vector<MSSpectrumType>& m);
   void setFilter(MSSpectrumType m);
 
   //For RAW files
-  bool lookupRT(char* c, int scanNum, float& rt);
-  void setAverageRaw(bool b, int width=1, long cutoff=1000);
-  void setLabel(bool b);  //label data contains all centroids (including noise and excluded peaks)
-  void setRawFilter(char* c);
-  void setRawFilterExact(bool b);
+  //bool lookupRT(char* c, int scanNum, float& rt);
+  //void setAverageRaw(bool b, int width=1, long cutoff=1000);
+  //void setLabel(bool b);  //label data contains all centroids (including noise and excluded peaks)
+  //void setRawFilter(char* c);
+  //void setRawFilterExact(bool b);
 
   //For MGF files
   void setHighResMGF(bool b);
@@ -118,7 +118,7 @@ class MSReader {
   void setCompression(bool b);
 
   //for Sqlite
-  void createIndex(); 
+  void createIndex();
 
  protected:
 
@@ -153,7 +153,7 @@ class MSReader {
   vector<MSSpectrumType> filter;
 
   //for RAW file support (even if not on windows)
-  bool rawFileOpen;
+  //bool rawFileOpen;
 
   //for mgf support
   char strMGF[1024];
@@ -174,11 +174,11 @@ class MSReader {
   void writeCompressSpec(FILE* fileOut, Spectrum& s);
   void writeTextSpec(FILE* fileOut, Spectrum& s);
   void writeSpecHeader(FILE* fileOut, bool text, Spectrum& s);
-  
+
   //support for rawfiles
   #ifdef _MSC_VER
   #ifndef _NO_THERMORAW
-	RAWReader cRAW;
+//	RAWReader cRAW;
   #endif
   #endif
 
@@ -196,11 +196,10 @@ class MSReader {
   void writeSqlite(const char* c, MSObject& m, char* sha1Report);
   void readChargeTable(int scanID, Spectrum& s);
   vector<int> estimateCharge(Spectrum& s);
-  #endif 
+  #endif
 
 };
 
 }
 
 #endif
-
